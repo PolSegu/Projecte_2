0e330659b594ff79a06b5db7ddaee9ea7d3f3f1a
Pol <pol.segu@enti.cat>
2020-11-28 11:13:16 +0100

    QuitButton

--- Unity/Public Politics/Assets/Project/Scripts/SceneController.cs
+++ Unity/Public Politics/Assets/Project/Scripts/SceneController.cs
@@ -23,4 +23,8 @@ public class SceneController : MonoBehaviour
         actualSceneNumero = SceneManager.GetActiveScene().buildIndex;
     }
     
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }
