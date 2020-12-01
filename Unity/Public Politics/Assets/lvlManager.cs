3023ca1719c041e35b3cc387b875514b72ba0e7f
Pol <pol.segu@enti.cat>
2020-12-01 07:37:33 +0100

    Remake of all gameLoop

---
+++ Unity/Public Politics/Assets/lvlManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvlManager : MonoBehaviour
+{
+    public static int time;
+    public static int dinero;
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+
+
+
+    
+}
