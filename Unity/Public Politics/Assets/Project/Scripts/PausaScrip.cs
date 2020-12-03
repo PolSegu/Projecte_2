808b6b09d23eb3143a368e1c1cdf84032c6a8ecd
Pol <pol.segu@enti.cat>
2020-12-03 14:32:36 +0100

    Programación

---
+++ Unity/Public Politics/Assets/Project/Scripts/PausaScrip.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PausaScrip : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pausaJuego;
+
+
+    void Start()
+    {
+        pausaJuego.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausaJuego.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pausaJuego.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+}
---
+++ Unity/Public Politics/Assets/Project/Scripts/lvlManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class lvlManager : MonoBehaviour
+{
+    public int time;
+    public int dinero;
+    public int vidas;
+    public int box;
+    public TextMeshProUGUI dineroAmount;
+    public GameObject pantallaGameOver;
+
+    GameObject FinalBueno;
+    GameObject GameOver;
+    
+    
+    void Start()
+    {
+        if(vidas <= 0)
+        {
+            pantallaGameOver.SetActive(true);
+        }
+        else
+        {
+            pantallaGameOver.SetActive(false);
+        }
+        
+    }
+
+    /*IEnumerator Esperar()
+    {
+
+        yield return new WaitForSeconds(0.3f);
+
+    }*/
+
+    void Update()
+    {
+        if(vidas <= 0)
+        {
+            pantallaGameOver.SetActive(true);
+            Debug.Log("gameover");
+        }
+        else
+        {
+            dineroAmount.text = "Dinero: " + dinero.ToString();
+        }
+    }
+
+    public void NextBox(Button button)
+    {
+        if(box == 1 || box == 2 || box == 3 || box == 7 || box == 9)
+        {
+            if(button.name == "AceptarButton")
+            {
+                dinero += 100;
+                Debug.Log("dinero");
+            }
+            if (button.name == "DenegarButton")
+            {
+                vidas--;
+                Debug.Log("-Vida");
+            }
+            
+        }
+
+        if (box == 4 || box == 5 || box == 6 || box == 8 || box == 10)
+        {
+            if (button.name == "DenegarButton")
+            {
+                dinero += 100;
+            }
+            if (button.name == "AceptarButton")
+            {
+                vidas--;
+            }
+            
+        }
+        box++;
+
+    }
+
+
+
+    
+
+}
