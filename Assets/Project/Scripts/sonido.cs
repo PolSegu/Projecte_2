4065099cfa6594aab007d54475389689ead8acac
PolSegu <pol.segu@enti.cat>
2021-01-23 23:50:17 +0100

    Sounds

--- Assets/Project/Scripts/sonido.cs
+++ Assets/Project/Scripts/sonido.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class sonido : MonoBehaviour
 {
-    public AudioSource fuente;
-    public AudioClip clip;
+    public AudioSource fuente1;
+    public AudioSource fuente2;
+    public AudioSource fuente3;
+
+    public AudioClip clipPapel;
+    public AudioClip clipCarpeta;
+    public AudioClip clipLupa;
 
     // Start is called before the first frame update
     void Start()
     {
-        fuente.clip = clip;
+        fuente1.clip = clipPapel;
+        fuente2.clip = clipCarpeta;
+        fuente3.clip = clipLupa;
     }
 
     // Update is called once per frame
-    public void Reproducir()
+    public void ReproducirPapel()
     {
-        clip.Play();
+        fuente1.Play();
+    }
+
+    public void ReproducirCarpeta()
+    {
+        fuente2.Play();
+    }
+
+    public void ReproducirLupa()
+    {
+        fuente3.Play();
     }
 }
