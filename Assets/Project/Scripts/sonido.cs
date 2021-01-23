954853f2ed22198c22f60a3e2ea63a3aac67f534
PolSegu <pol.segu@enti.cat>
2021-01-24 00:32:53 +0100

    Soundtrack

--- Assets/Project/Scripts/sonido.cs
+++ Assets/Project/Scripts/sonido.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class sonido : MonoBehaviour
 {
+    // Sound events
+
     public AudioSource fuente1;
     public AudioSource fuente2;
     public AudioSource fuente3;
+    public AudioSource fuente4;
+    public AudioSource fuente5;
 
     public AudioClip clipPapel;
     public AudioClip clipCarpeta;
     public AudioClip clipLupa;
+    public AudioClip clipContainerLargo;
 
+    // Soundtrack
+
+    public AudioSource fuente20;
+    public AudioClip musicBackground1;
+    
     // Start is called before the first frame update
     void Start()
     {
         fuente1.clip = clipPapel;
         fuente2.clip = clipCarpeta;
         fuente3.clip = clipLupa;
+        fuente4.clip = clipContainerLargo;
+        fuente5.clip = clipContainerLargo;
+
+        fuente20.clip = musicBackground1;
     }
 
     // Update is called once per frame
@@ -34,5 +48,31 @@ public class sonido : MonoBehaviour
     public void ReproducirLupa()
     {
         fuente3.Play();
+    }
+
+    public void ReproducirContainerLargo()
+    {
+        fuente4.Play();
+    }
+
+    public void ReproducirContainerLargoVuelta()
+    {
+        fuente5.Play();
+    }
+
+    public void PlayMusicaFondo()
+    {
+        // Music Background
+
+        fuente20.Play();
+
+    }
+
+    public void StopMusicaFondo()
+    {
+        // Music Background
+
+        fuente20.Stop();
+
     }
 }
