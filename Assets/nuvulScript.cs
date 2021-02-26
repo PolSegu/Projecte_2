4855c80a26ee903972d8c1e2918f134a8ad21381
PolSegu <pol.segu@enti.cat>
2021-02-26 19:10:18 +0100

    Lvl art done

--- Assets/nuvulScript.cs
+++ Assets/nuvulScript.cs
@@ -8,24 +8,36 @@ public class nuvulScript : MonoBehaviour
     public float speed;
     public Transform startLocation;
     public Transform FinishLocation;
+    public int Case = 0;
+
 
 
     // Update is called once per frame
     void Update()
     {
-        
 
-        if(transform.position.x <= FinishLocation.position.x)
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+        if (Case == 0) { 
+            if(transform.position.x <= FinishLocation.position.x)
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
             
+            }
+            else
+            {
+                transform.position = startLocation.position;
+            }
         }
-        else
+        if (Case == 1)
         {
-            transform.position = startLocation.position;
+            if (transform.position.x >= FinishLocation.position.x)
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+
+            }
+            else
+            {
+                transform.position = startLocation.position;
+            }
         }
-
-
-
     }
 }
